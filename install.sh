d2014075ae3e6119ce13a9bb086981910c211ded
dian113 <mohammaddianpratama@gmail.com>
2025-06-02 04:50:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -155,7 +155,7 @@ echo -e "${GREEN}===============================================================
 echo -e "${GREEN}========== GenieACS UI akses port 3000. : http://$local_ip:3000 ============${NC}"
 echo -e "${GREEN}=================== Informasi: Whatsapp 081947215703 =======================${NC}"
 echo -e "${GREEN}============================================================================${NC}"
-cp -r logo-3976e73d.svg /usr/lib/node_modules/genieacs/public/
+cp -r WhatsApp Image 2025-06-02 at 04.48.05.jpeg /usr/lib/node_modules/genieacs/public/
 echo -e "${GREEN}Sekarang install parameter. Apakah anda ingin melanjutkan? (y/n)${NC}"
 read confirmation
 
